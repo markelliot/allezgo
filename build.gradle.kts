7f0ba9036a64658bf3b50eae4e104d89d039f47a
mellbot <94683073+mellbot@users.noreply.github.com>
2022-10-11 08:16:38 -0400

    Auto-update dependencies and plugins (#85)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
     idea
     id("com.diffplug.spotless") version "6.11.0"
     id("com.google.cloud.tools.jib") version "3.3.0" apply false
-    id("com.markelliot.versions") version "0.21.0"
+    id("com.markelliot.versions") version "0.22.0"
     id("com.palantir.consistent-versions") version "2.11.0"
-    id("net.ltgt.errorprone") version "3.0.0" apply false
+    id("net.ltgt.errorprone") version "3.0.1" apply false
     id("org.inferred.processors") version "3.7.0" apply false
 }
 
